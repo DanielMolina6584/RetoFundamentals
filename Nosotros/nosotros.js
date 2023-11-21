eb3cc2d3576b8084c28d730a257219c9ec39e573
Daniel Molina <daniel.torres312@gmail.com>
2023-11-21 13:10:48 -0500

    Creacion header y Nav

---
+++ Nosotros/nosotros.js
@@ -0,0 +1,8 @@
+function mostrar(){
+   const sidebar = document.querySelector('.sidebar');
+   sidebar.style.display='flex';
+}
+function quitar(){
+    const quitar = document.querySelector('.sidebar');
+    quitar.style.display = 'none';
+}
