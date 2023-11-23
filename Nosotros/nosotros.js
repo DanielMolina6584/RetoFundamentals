bcc41d2f1b972615dea86d4ab06b9bbf1f04e76f
Daniel Molina <daniel.torres312@gmail.com>
2023-11-23 16:57:53 -0500

    Terminacion Todo Contenido Principal

--- Nosotros/nosotros.js
+++ Nosotros/nosotros.js
@@ -1,8 +1,38 @@
-function mostrar(){
-   const sidebar = document.querySelector('.sidebar');
-   sidebar.style.display='flex';
+import { data } from "./data.js";
+
+const carrusel = document.getElementById('carrusel');
+const flechaI = document.querySelector('.flechaI');
+const flechaD = document.querySelector('.flechaD');
+
+let actual = 0;
+
+function MostrarCarrusel(index) {
+   
+    const ahora = data[index];
+    carrusel.innerHTML = `
+        <div class = "slide">
+        <h3 class="sesion3_todo_titulo">${ahora.Titulo}</h3>
+        <img src="${ahora.Imagen}" alt="" class="sesion3_todo_imagen">
+        <p class="sesion3_todo_parrafo">${ahora.Texto}</p>
+        
+        </div>
+    `;
+ actual = index
+   
 }
-function quitar(){
-    const quitar = document.querySelector('.sidebar');
-    quitar.style.display = 'none';
+function atras() {
+    actual = (actual + 1) % data.length;
+    MostrarCarrusel(actual);
 }
+
+function adelante() {
+    actual = (actual - 1 + data.length) % data.length
+    MostrarCarrusel(actual);
+    
+}
+
+flechaI.addEventListener('click', atras);
+flechaD.addEventListener('click', adelante);
+
+MostrarCarrusel(actual)
+
---
+++ Nosotros/script.js
@@ -0,0 +1,8 @@
+function abrir(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display='flex';
+ }
+ function quitar(){
+     const quitar = document.querySelector('.sidebar');
+     quitar.style.display = 'none';
+ }
