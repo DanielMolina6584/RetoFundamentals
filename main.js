b7eea6bfb221fd5e2a9b5eb639f40368ab1757fb
Madrigal <dmadrigalv07@gmail.com>
2023-11-27 23:10:44 -0500

    Sprint_2

--- main.js
+++ main.js
@@ -15,3 +15,16 @@ function validar() {
   function redirigir() {
     window.location.href = 'inicio.html'; 
   }
+
+
+  const menuBtn = document.getElementById('menubtn');
+  const menu = document.querySelector('.menu');
+  
+  menuBtn.addEventListener('click', function() {
+    menu.classList.toggle('show-menu');
+  });
+
+document.querySelector('.horizontal-scroll').addEventListener('wheel', function(e) {
+  e.preventDefault();
+  this.scrollLeft += e.deltaY * 1; // Cambia el valor de '1' para ajustar la velocidad del scroll horizontal
+});
