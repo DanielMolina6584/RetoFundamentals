58aece6627eb776156d747471f8a445befa06678
Madrigal <dmadrigalv07@gmail.com>
2023-12-02 13:06:51 -0500

    avance

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function validar() {
+    var user = document.getElementById('user').value;
+    var pswd = document.getElementById('pswd').value;
+
+    if (user === 'admin' && pswd === '1234') {
+      alert('Inicio de sesión exitoso');
+      redirigir();
+      return false; 
+    } else {
+      alert('Credenciales incorrectas. Inténtalo de nuevo.');
+      return false;
+    }
+  }
+  
+  function redirigir() {
+    window.location.href = 'inicio.html'; 
+  }
+
+
+  const menuBtn = document.getElementById('menubtn');
+  const menu = document.querySelector('.menu');
+  
+  menuBtn.addEventListener('click', function() {
+    menu.classList.toggle('show-menu');
+  });
